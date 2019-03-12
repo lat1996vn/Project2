199ed6af04050b7371c498ccab98cb3cdce96843
Lat <tuan.la2611@gmail.com>
2019-03-13 05:08:30 +0700

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Lat on 3/13/19.
+//  Copyright © 2019 LAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
