1f50e76f5b0714209aedf957ca58eaa8a944f1e4
Lat <tuan.la2611@gmail.com>
2019-03-13 22:45:17 +0700

    Design layout, add flag image

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnFlag1: UIButton!
+    @IBOutlet weak var btnFlag2: UIButton!
+    @IBOutlet weak var btnFlag3: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
